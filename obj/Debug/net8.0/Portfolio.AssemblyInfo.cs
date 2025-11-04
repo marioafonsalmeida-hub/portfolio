09447a57b9ac8d411be3ce1efaba1398f51409e8
Paulo Dias <pj.dias@activex.pt>
2025-11-04 15:01:02 +0000

    Fix Program

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fa79def2297ace752daa709bd0df24360cee43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044de09ffe5bc3ccdd5353b1756596d7667f8375")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
