c26c60cebbff2e8250a34b8eb755590536536d4c
Paulo Dias <pj.dias@activex.pt>
2025-11-04 15:29:55 +0000

    Changed Program

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles(); // <-- enable serving files from wwwroot
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
